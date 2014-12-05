5fd2ede3b73d5e45c72ada8b10ea410b163dbd60
Jonathan Wehrle <jonathanw@gmx.de>
2014-12-05 18:32:34 +0100

    Write to files

--- xcobb.cpp
+++ xcobb.cpp
@@ -10,6 +10,8 @@ namespace fs = boost::filesystem;
 
 int main ()
 {
+	fs::path working_dir("../xcom/build");
+
 	ifstream obbfile;
 	obbfile.open("../main.12.com.tt2kgames.xcomew.obb", ios::in | ios::binary);
 
@@ -36,19 +38,20 @@ int main ()
 
 	int namelength = 0;
 	int listpos = 0;
-	for (int i = 0; i < 1; i++ )//filecount; i++)
+	for (int i = 0; i < filecount; i++)
 	{
 		obbfile.read( (char*)&namelength, 4);
 		buffer = new char[namelength];
 		obbfile.read( buffer, namelength);
 		cout << "  " << buffer << endl;
+
 		//4B file name length | filename (variable) | 8B pos | 4B length
 		long position = 0;
 		int length = 0;
 		obbfile.read( (char*)&position, 8);
 		obbfile.read( (char*)&length, 4);
-	
-		//listpos = obbfile.tellg();	
+
+		listpos = obbfile.tellg();	
 	
 		for ( int j = 0; j < namelength; j++)
 		{
@@ -57,12 +60,33 @@ int main ()
 				buffer[j] = '/';
 			}
 		}
-		
-		fs::path path (buffer);
-		if ( ! fs::create_directories(path) )
-			cout << "Could not create directory: " << path.string() << endl;
 
-		//obbfile.seekg(listpos);
+		fs::path path ( working_dir / buffer);
+		
+		if ( ! fs::create_directories(path.parent_path()) )
+			cout << "Could not create directory: " << path.parent_path().string() << endl;
+
+		obbfile.seekg(position);
+		ofstream of;
+		of.open(path.string().c_str(), ios::out | ios::binary);
+		
+		int buffer_size = 500000;
+		int progress = 0;
+
+		buffer = new char[buffer_size];
+		while ( progress < length)
+		{
+			int cbuf = buffer_size;
+			if ( progress + buffer_size > length )
+				cbuf = length - progress;
+
+			obbfile.read(buffer, cbuf);
+			of.write(buffer, cbuf);
+			progress += cbuf;
+		}
+
+		//reset list
+		obbfile.seekg(listpos);
 	}
 
 	obbfile.close();
